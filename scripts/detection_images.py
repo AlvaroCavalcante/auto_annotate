caa2d38cb0b6616fd0e93d418551ed70fb4563d9
Alvarole <leandro0807@live.com>
2019-12-23 17:54:57 -0300

    first commit

---
+++ scripts/detection_images.py
@@ -0,0 +1,151 @@
+from object_detection.utils import visualization_utils as vis_util
+from object_detection.utils import label_map_util
+from object_detection.utils import ops as utils_ops
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+import zipfile
+
+from collections import defaultdict
+from io import StringIO
+from matplotlib import pyplot as plt
+from PIL import Image
+import os
+import glob
+
+# Path to frozen detection graph. This is the actual model that is used for the object detection.
+PATH_TO_CKPT = './graphs/frozen_inference_graph.pb'
+
+# List of the strings that is used to add correct label for each box.
+PATH_TO_LABELS = './graphs/label_map.pbtxt'
+
+# Path to the images you want to infer
+PATH_TO_TEST_IMAGES_DIR = './images'
+
+assert os.path.isfile('./graphs/frozen_inference_graph.pb')
+assert os.path.isfile(PATH_TO_LABELS)
+
+TEST_IMAGE_PATHS = glob.glob(os.path.join(PATH_TO_TEST_IMAGES_DIR, "*.*"))
+assert len(TEST_IMAGE_PATHS) > 0, 'No image found in `{}`.'.format(
+    PATH_TO_TEST_IMAGES_DIR)
+
+try:
+
+    detection_graph = tf.Graph()
+    with detection_graph.as_default():
+        od_graph_def = tf.GraphDef()
+        with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+            serialized_graph = fid.read()
+            od_graph_def.ParseFromString(serialized_graph)
+            tf.import_graph_def(od_graph_def, name='')
+
+    label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+    categories = label_map_util.convert_label_map_to_categories(
+        label_map, max_num_classes=2, use_display_name=True)
+    category_index = label_map_util.create_category_index(categories)
+
+    def load_image_into_numpy_array(image):
+        (im_width, im_height) = image.size
+        return np.array(image.getdata()).reshape(
+            (im_height, im_width, 3)).astype(np.uint8)
+
+    def get_files_on_directory(path):
+        directory = os.path.basename(path)
+        file_list = os.listdir(directory)
+        return file_list
+
+    def run_inference_for_single_image(image, graph):
+        with graph.as_default():
+            with tf.Session() as sess:
+                # Get handles to input and output tensors
+                ops = tf.get_default_graph().get_operations()
+                all_tensor_names = {
+                    output.name for op in ops for output in op.outputs}
+                tensor_dict = {}
+                for key in [
+                    'num_detections', 'detection_boxes', 'detection_scores',
+                    'detection_classes', 'detection_masks'
+                ]:
+                    tensor_name = key + ':0'
+                    if tensor_name in all_tensor_names:
+                        tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
+                            tensor_name)
+                if 'detection_masks' in tensor_dict:
+                    # The following processing is only for single image
+                    detection_boxes = tf.squeeze(
+                        tensor_dict['detection_boxes'], [0])
+                    detection_masks = tf.squeeze(
+                        tensor_dict['detection_masks'], [0])
+                    # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
+                    real_num_detection = tf.cast(
+                        tensor_dict['num_detections'][0], tf.int32)
+                    detection_boxes = tf.slice(detection_boxes, [0, 0], [
+                        real_num_detection, -1])
+                    detection_masks = tf.slice(detection_masks, [0, 0, 0], [
+                        real_num_detection, -1, -1])
+                    detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
+                        detection_masks, detection_boxes, image.shape[0], image.shape[1])
+                    detection_masks_reframed = tf.cast(
+                        tf.greater(detection_masks_reframed, 0.5), tf.uint8)
+                    # Follow the convention by adding back the batch dimension
+                    tensor_dict['detection_masks'] = tf.expand_dims(
+                        detection_masks_reframed, 0)
+                image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+
+                # Run inference
+                output_dict = sess.run(tensor_dict,
+                                       feed_dict={image_tensor: np.expand_dims(image, 0)})
+
+                # all outputs are float32 numpy arrays, so convert types as appropriate
+                output_dict['num_detections'] = int(
+                    output_dict['num_detections'][0])
+                output_dict['detection_classes'] = output_dict[
+                    'detection_classes'][0].astype(np.uint8)
+                output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
+                output_dict['detection_scores'] = output_dict['detection_scores'][0]
+                if 'detection_masks' in output_dict:
+                    output_dict['detection_masks'] = output_dict['detection_masks'][0]
+        return output_dict
+
+    count = 0
+    for image_path in TEST_IMAGE_PATHS:
+        image = Image.open(image_path)
+
+        im_width, im_height = image.size
+
+        im_width_inche = im_width // 77
+        im_height_inche = im_height // 77 #redimensioning the image resolution
+
+        IMAGE_SIZE = (im_width_inche, im_height_inche)
+
+        # the array based representation of the image will be used later in order to prepare the
+        # result image with boxes and labels on it.
+        image_np = load_image_into_numpy_array(image)
+        # Expand dimensions since the model expects images to have e:[1 shap, None, None, 3]
+        image_np_expanded = np.expand_dims(image_np, axis=0)
+        # Actual detection.
+        output_dict = run_inference_for_single_image(image_np, detection_graph)
+
+        # Visualization of the results of a detection.
+        vis_util.visualize_boxes_and_labels_on_image_array(
+            image_np,
+            output_dict['detection_boxes'],
+            output_dict['detection_classes'],
+            output_dict['detection_scores'],
+            category_index,
+            instance_masks=output_dict.get('detection_masks'),
+            use_normalized_coordinates=True,
+            line_thickness=10)
+
+        plt.figure(figsize=IMAGE_SIZE)
+        plt.axis('off')
+        plt.imshow(image_np)
+
+        plt.savefig('./results/image_' + str(count) + '.jpg', bbox_inches='tight')
+        count += 1
+
+except Exception as error:
+    print(error)
---
+++ scripts/generate_xml.py
@@ -0,0 +1,48 @@
+import xml.etree.cElementTree as ET
+from random import randrange
+import os
+
+class GenerateXml(object):
+    def __init__(self, box_array, im_width, im_height, classe):
+        self.classe = classe
+        self.box_array = box_array
+        self.im_width = im_width
+        self.im_height = im_height
+
+    def get_file_name(self):
+        xml_path = '/home/alvaro/auto_annotate/xml'
+        #directory = os.path.basename(xml_path)
+        #file_list = os.listdir(directory)
+        print(os.path.dirname(os.path.abspath(__file__)) )
+        # if len(file_list) == 0:
+        #     return 1
+        # else:
+        #     return len(file_list) + 1
+
+    def gerenate_basic_structure(self):
+        file_name = "image" + "_" + str(self.get_file_name())
+        annotation = ET.Element("annotation")
+        ET.SubElement(annotation, "filename").text = file_name + ".jpg"
+        size = ET.SubElement(annotation, "size")
+        ET.SubElement(size, "width").text = str(self.im_width)
+        ET.SubElement(size, "height").text = str(self.im_height)
+        ET.SubElement(size, "depth").text = "3"
+        
+        for i in self.box_array:
+            objectBox = ET.SubElement(annotation, "object")
+            ET.SubElement(objectBox, "name").text = self.classe
+            ET.SubElement(objectBox, "pose").text = "Unspecified"
+            ET.SubElement(objectBox, "truncated").text = "0"
+            ET.SubElement(objectBox, "difficult").text = "0"
+            bndBox = ET.SubElement(objectBox, "bndbox")
+            ET.SubElement(bndBox, "xmin").text = str(i['xmin'])
+            ET.SubElement(bndBox, "ymin").text = str(i['ymin'])
+            ET.SubElement(bndBox, "xmax").text = str(i['xmax'])
+            ET.SubElement(bndBox, "ymax").text = str(i['ymax'])
+
+        arquivo = ET.ElementTree(annotation)
+        arquivo.write("/home/alvaro/Coffee_Recognize_API/xml/" + file_name + ".xml")
+
+def main():
+    xml = GenerateXml([{'xmin': 0.5406094193458557, 'xmax': 0.6001364588737488, 'ymin': 0.6876631379127502, 'ymax': 0.7547240853309631}, {'xmin': 0.5406094193458557, 'xmax': 0.6001364588737488, 'ymin': 0.6876631379127502, 'ymax': 0.7547240853309631}, {'xmin': 0.5406094193458557, 'xmax': 0.6001364588737488, 'ymin': 0.6876631379127502, 'ymax': 0.7547240853309631}, {'xmin': 0.5406094193458557, 'xmax': 0.6001364588737488, 'ymin': 0.6876631379127502, 'ymax': 0.7547240853309631}], '4000', '2000', 'bicho_mineiro')
+    xml.gerenate_basic_structure()    
