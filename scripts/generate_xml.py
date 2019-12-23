d2b98f30b9d0507087feea99cb90fb73467c52f9
Alvarole <leandro0807@live.com>
2019-12-23 20:55:52 -0300

    changed the paths

--- scripts/generate_xml.py
+++ scripts/generate_xml.py
@@ -11,13 +11,13 @@ class GenerateXml(object):
 
     def get_file_name(self):
         xml_path = '/home/alvaro/auto_annotate/xml'
-        #directory = os.path.basename(xml_path)
-        #file_list = os.listdir(directory)
-        print(os.path.dirname(os.path.abspath(__file__)) )
-        # if len(file_list) == 0:
-        #     return 1
-        # else:
-        #     return len(file_list) + 1
+        directory = os.path.basename(xml_path)
+        file_list = os.listdir(directory)
+
+        if len(file_list) == 0:
+            return 1
+        else:
+            return len(file_list) + 1
 
     def gerenate_basic_structure(self):
         file_name = "image" + "_" + str(self.get_file_name())
@@ -41,7 +41,7 @@ class GenerateXml(object):
             ET.SubElement(bndBox, "ymax").text = str(i['ymax'])
 
         arquivo = ET.ElementTree(annotation)
-        arquivo.write("/home/alvaro/Coffee_Recognize_API/xml/" + file_name + ".xml")
+        arquivo.write("/home/alvaro/auto_annotate/xml/" + file_name + ".xml")
 
 def main():
     xml = GenerateXml([{'xmin': 0.5406094193458557, 'xmax': 0.6001364588737488, 'ymin': 0.6876631379127502, 'ymax': 0.7547240853309631}, {'xmin': 0.5406094193458557, 'xmax': 0.6001364588737488, 'ymin': 0.6876631379127502, 'ymax': 0.7547240853309631}, {'xmin': 0.5406094193458557, 'xmax': 0.6001364588737488, 'ymin': 0.6876631379127502, 'ymax': 0.7547240853309631}, {'xmin': 0.5406094193458557, 'xmax': 0.6001364588737488, 'ymin': 0.6876631379127502, 'ymax': 0.7547240853309631}], '4000', '2000', 'bicho_mineiro')
